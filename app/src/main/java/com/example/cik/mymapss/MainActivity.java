9217ed9e08069f23bf1caa7c6065baf9a06f0e7d
cikmon <cikmon@mail.ru>
2017-05-23 19:13:39 +0300

    1233

--- app/src/main/java/com/example/cik/mymapss/MainActivity.java
+++ app/src/main/java/com/example/cik/mymapss/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.cik.mymapss;
 
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FloatingActionButton map = (FloatingActionButton) findViewById(R.id.fab);
+
+       /* map.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                Toast toast = Toast.makeText(this, "Hello Android 7",Toast.LENGTH_LONG);
+                toast.show();
+
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                 //       .setAction("Action", null).show();
+            }
+        });
+*/
+
+
+}
+
+
+    public void onClick12(View view) {
+        Toast toast = Toast.makeText(this, "Hello Android 7",Toast.LENGTH_LONG);
+        toast.show();
     }
 }
+
